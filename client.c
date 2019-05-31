b6b113a0deb058600a18246c2d088e449894cae2
lio <Lzh1602694338@outlook.com>
2019-05-31 08:26:32 +0800

    new usable core

--- client.c
+++ client.c
@@ -1,49 +1,174 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<netinet/in.h>
-#define PORT 8888
-#define MAXDATASIZE 2048
+/*client函数实现向server端发送昵称并登入server功能，登入后可进行并发消息操作，输入 ：q 退出聊天
+这里默认是主机内程序互相通信，故serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");这里直接使用了127.0.0.1的ip地址
+在实际使用过程中，以及多人聊天时，要将此处的IP地址改为变量，同时用以辨识不同客户*/ 
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
+#include <stdio.h>
+
+#include <errno.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+#include <netdb.h>
+
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <sys/types.h>
+
+#include <arpa/inet.h>
+
+#include <pthread.h>
+
+ 
+
+#define MAXLINE 100;
+
+void *threadsend(void *vargp);
+
+void *threadrecv(void *vargp);
+//定义全局变量，作用分别为接收信息、发送信息、发送昵称、接收昵称 
+char temprecv[100];
+char tempsend[100];
+char namesend[100]={'\0'};
+char namerecv[100]={'\0'};
+
+int main()
+
 {
-    if(argc != 2)
-    {
-        fprintf(stderr, "Please input the IP address!\n");
+int *clientfdp;
+struct sockaddr_in serveraddr;
+pthread_t tid1,tid2;
+int flag=1;
+
+clientfdp = (int *)malloc(sizeof(int));
+
+*clientfdp = socket(AF_INET,SOCK_STREAM,0);
+
+bzero((char *)&serveraddr,sizeof(serveraddr));
+
+serveraddr.sin_family = AF_INET;
+
+serveraddr.sin_port = htons(15636);
+
+serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+if(connect(*clientfdp,(struct sockaddr *)&serveraddr,sizeof(serveraddr)) < 0){
+
+        printf("connect error\n");
+
         exit(1);
-    }
-    int sockfd;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    const char *server_ip = argv[1]; 
-    struct sockaddr_in serveraddr;
-    bzero(&serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(PORT);
-    inet_pton(AF_INET, server_ip, &serveraddr.sin_addr);
-    connect(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
-    printf("=======================server connect successfully!=====================\n");
-    char buf[MAXDATASIZE];
-    memset(buf, 0 ,  sizeof(buf));
-    printf("input: ");
-    while(fgets(buf, sizeof(buf), stdin) != NULL)
-    {
-	if(buf[0]==':'&&buf[1]=='q')
-	{
-		printf("client will be closed, see you next time.\n");
-		return 0;
-	}
-        send(sockfd, buf, sizeof(buf), 0);
-        memset(buf, 0, sizeof(buf));
-        recv(sockfd, buf, sizeof(buf), 0);
-        printf("server say: ");
-        fputs(buf, stdout);
-        memset(buf, 0, sizeof(buf));
-        printf("input: ");
-    }
-    printf("client will be closed, see you next time.\n");
-    close(sockfd);
-    return 0;
+
 }
+
+printf("connected\n");
+
+//此处用以判断昵称是否重复，须在server函数对应位置进行多对多修改 
+while(flag){
+printf("Please input your username:\n");
+fgets(namesend,100,stdin);
+send(*clientfdp,namesend,100,0);
+recv(*clientfdp,namerecv,100,0);
+flag=strcmp(namesend,namerecv);
+}
+printf("welcome,%s\n",namesend);
+
+//主循环 
+while(1){
+
+pthread_create(&tid1,NULL,threadsend,clientfdp);
+
+if(tempsend[0]==':' && tempsend[1]=='q'){
+	printf("client quit successfully!\n");
+	close(*clientfdp);
+	return 0;
+}
+if(temprecv[0]==':' && temprecv[1]=='q'){
+	printf("server loss connection\n");
+	return 0;
+}
+pthread_create(&tid2,NULL,threadrecv,clientfdp);
+
+}
+
+
+
+return EXIT_SUCCESS;
+
+}
+
+
+//发送信息线程 
+void *threadsend(void * vargp)
+
+{
+
+//pthread_t tid2;
+
+int connfd = *((int *)vargp);
+
+
+
+int idata;
+
+
+while(1){
+
+//printf("me: \n ");
+
+fgets(tempsend,100,stdin);
+
+send(connfd,tempsend,100,0);
+if(tempsend[0]==':' && tempsend[1]=='q'){
+	return 0;
+}
+
+memset(tempsend,'\0',100);
+printf("----------------%s\n",namesend);
+
+
+
+}
+
+return NULL;
+
+}
+
+
+
+
+//接收信息线程 
+void *threadrecv(void *vargp)
+
+{
+
+
+int connfd = *((int *)vargp);
+
+while(1){
+
+int idata = 0;
+
+idata = recv(connfd,temprecv,100,0);
+if(temprecv[0]==':' && temprecv[1]=='q'){
+	return 0;
+}
+if(idata > 0){
+
+printf("server :%s\n",temprecv);
+
+}
+memset(temprecv,'\0',100);
+}
+
+ 
+
+ 
+
+return NULL;
+
+}
+
--- server.c
+++ server.c
@@ -1,56 +1,209 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
+ /*此函数用于服务器端，暂时实现了退出功能，但判断昵称是否重复以及实现多人聊天并未实现
+ 可采取客服发往服务器，服务器转发消息至所有人的思路，即创建一个多维数组用以存储每个人发送到服务器的消息
+ ，然后同时转发这些消息到所有人（除发送者）*/
+#include <stdlib.h>
 
-#define PORT 8888
-#define BACKLOG 10
-#define MAXDATASIZE  2048
+#include <stdio.h>
 
-int main(int argc, char *argv[])
+#include <errno.h>
+
+#include <string.h>
+ 
+#include <unistd.h>
+
+#include <netdb.h>
+
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <sys/types.h>
+
+#include <arpa/inet.h>
+
+#include <pthread.h>
+
+#define PORT 15636
+
+//void *thread(void *vargp);
+
+void *threadsend(void *vargp);
+
+void *threadrecv(void *vargp);
+
+ 
+char temprecv[100];
+char tempsend[100];
+char namesend[100]={'\0'};
+char namerecv[100]={'\0'};
+
+int main()
+
 {
-int listenfd;
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    printf("listenfd=%d\n", listenfd);
-    struct sockaddr_in serveraddr;
-    bzero(&serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(PORT); 
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
-    bind(listenfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
-    listen(listenfd, BACKLOG);
-    printf("======bind success,waiting for client's request======\n");
-    struct sockaddr_in peeraddr;
-    socklen_t peer_len = sizeof(peeraddr);
-    int connfd;
-    while(1)
-    {
-        connfd = accept(listenfd, (struct sockaddr*)&peeraddr, &peer_len);
-        printf("\n=====================client connect successfully!=====================\n");
-        printf("IP = %s:PORT = %d\n", inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port));
-        char buf[MAXDATASIZE];
-        while(1)
-        {
-            memset(buf, '\0', MAXDATASIZE/sizeof  (char));
-            int recv_length = recv(connfd, buf, MAXDATASIZE/sizeof (char), 0);
-            if(recv_length == 0)
-            {
-                printf("client has closed!\n");
-                break;
-            }
-            printf("client say: ");
-            fputs(buf, stdout);
-            memset(buf, '\0', MAXDATASIZE/sizeof (char));
-            printf("input: ");
-            fgets(buf, sizeof(buf), stdin);
-            send(connfd, buf, recv_length, 0);
-        }
-        close(connfd);
-        close(listenfd);
-        return 0;
-}}
+int namelen=0;
+struct sockaddr_in serveraddr; 
+struct sockaddr_in clientaddr;
+int clientlen, *connfd;
+
+int listenfd = socket(AF_INET, SOCK_STREAM,0);
+
+if(listenfd < 0){
+
+        perror("socket");
+
+        exit(1);
+
+}
+
+bzero((char *)&serveraddr,sizeof(serveraddr));
+
+serveraddr.sin_family = AF_INET;
+
+serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+serveraddr.sin_port = htons(PORT);
+
+
+
+if(bind(listenfd,(struct sockaddr *)&serveraddr,sizeof(serveraddr)) < 0){
+
+        perror("connect");
+
+        exit(1);
+
+}
+
+
+
+if(listen(listenfd,1024) < 0){
+
+        perror("listen error");
+
+        exit(1);
+
+}
+
+
+
+
+
+clientlen = sizeof(clientaddr);
+
+connfd = (int *)malloc(sizeof(int));
+
+*connfd = accept(listenfd,(struct sockaddr *)&clientaddr, &clientlen);
+
+printf("Accepted!\n");
+
+recv(*connfd,namerecv,100,0);
+strcpy(namesend,namerecv);
+send(*connfd,namesend,100,0);
+for(int i=0;i<100;i++){
+	if(namerecv[i]!='\0')
+	namelen++;
+}
+namerecv[namelen-1]='\0';
+printf("%s check in!\n",namerecv);
+
+while(1){
+	pthread_t tid1,tid2;
+	
+	pthread_create(&tid1,NULL,threadsend,connfd);
+	
+	if(tempsend[0]==':' && tempsend[1]== 'q'){
+		printf("server close successfully!\n");
+		close(*connfd);
+		close(listenfd);
+		return 0;
+	}
+	
+	if(temprecv[0]==':' && temprecv[1]== 'q'){
+		printf("client connection close\n");
+		return 0;
+	}
+	
+	pthread_create(&tid2,NULL,threadrecv,connfd);
+}
+return EXIT_SUCCESS;
+
+}
+
+
+
+//void *thread(void *vargp)
+//
+//{
+//
+//
+//
+//pthread_t tid1,tid2;
+//
+//
+//pthread_create(&tid1,NULL,threadsend,vargp);
+//
+//pthread_create(&tid2,NULL,threadrecv,vargp);
+//
+//return NULL;
+//
+//}
+
+
+
+void *threadsend(void * vargp)
+
+{
+
+
+
+int connfd = *((int *)vargp);
+
+
+while(1){
+	
+fgets(tempsend,100,stdin);
+
+send(connfd,tempsend,100,0);
+
+if(tempsend[0]==':' && tempsend[1]=='q'){
+	return NULL;
+}
+printf("----------------server\n");
+
+memset(tempsend,'\0',100);
+
+}
+
+return NULL;
+
+}
+
+
+
+void *threadrecv(void *vargp)
+
+{
+
+
+int connfd = *((int *)vargp);
+
+while(1){
+
+int idata = 0;
+
+idata = recv(connfd,temprecv,100,0);
+if(temprecv[0]==':' &&temprecv[1]=='q'){
+	return NULL;
+}
+if(idata > 0){
+
+printf("%s :%s\n",namerecv,temprecv);
+
+}
+
+memset(temprecv,'\0',100);
+}
+
+return NULL;
+
+}
 
